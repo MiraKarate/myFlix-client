4fa37ead1af3f05bf318113fef338463c061b861
MiraKarate <mira.herrarte@gmail.com>
2023-05-19 10:45:50 +0200

    add password hashing

--- models.js
+++ models.js
@@ -25,6 +25,13 @@ let movieSchema = mongoose.Schema({
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
   });
 
+  userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+  };
+  
+  userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+  };
   
   let Movie = mongoose.model('Movie', movieSchema);
   let User = mongoose.model('User', userSchema);
