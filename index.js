ccf6665db16d416bbd6388d0e6a8dec52984974d
MiraKarate <mira.herrarte@gmail.com>
2023-05-21 10:59:35 +0200

    Update index.js add validation to update user info and passport to endpoints

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ app.get('/movies/directors/:directorName', passport.authenticate('jwt', { sessio
 // USERS//
 
 // Get all users
-app.get('/users', (req, res) => {
+app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.find()
     .then((users) => {
       res.status(201).json(users);
@@ -132,7 +132,7 @@ app.get('/users', (req, res) => {
 });
 
 // Get a user by username
-app.get('/users/:Username', (req, res) => {
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findOne({ Username: req.params.Username })
     .then((user) => {
       res.json(user);
@@ -189,11 +189,19 @@ check('Username', 'Username contains non alphanumeric characters - not allowed.'
 check('Password', 'Password is required').not().isEmpty(),
 check('Email', 'Email does not appear to be valid').isEmail()
 ], passport.authenticate('jwt', {session: false}), (req, res) => {
+  // check the validation object for errors
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
+
   Users.findOneAndUpdate({ Username: req.params.Username }, {
     $set:
     {
       Username: req.body.Username,
-      Password: req.body.Password,
+      Password: hashedPassword,
       Email: req.body.Email,
       Birthday: req.body.Birthday
     }
